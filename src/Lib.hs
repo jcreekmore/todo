f6126210c5b991d303b7936c7e6e63b8cdebf90e
Jonathan Creekmore <jonathan@thecreekmores.org>
2016-02-18 19:38:17 -0600

    Extremely basic complete/incomplete todo parsing

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,33 @@
 module Lib
     ( someFunc
+    , Todo(..)
+    , priority
+    , parse
     ) where
+
+type Action = String
+type Priority = Char
+type Date = String
+type Context = String
+type Project = String
+
+data Todo = Incomplete Action (Maybe Priority) (Maybe Date) [Context] [Project]
+          | Complete Action Date (Maybe Date) [Context] [Project]
+          deriving(Eq, Show)
+
+priority :: Todo -> Maybe Priority
+priority (Incomplete _ p _ _ _) = p
+priority _ = Nothing
+
+parseComplete :: [String] -> (Bool, [String])
+parseComplete ("x":xs) = (True, xs)
+parseComplete xs = (False, xs)
+
+parse :: String -> Todo
+parse x
+      | isComplete = Complete (unwords $ tail action) (head action) Nothing [] []
+      | otherwise = Incomplete (unwords action) Nothing Nothing [] []
+      where (isComplete, action) = parseComplete $ words x
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,11 @@
+import Test.Hspec
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "Lib.parse" $ do
+        it "returns a basic Incomplete Todo" $ do
+            parse "A basic todo" `shouldBe` (Incomplete "A basic todo" Nothing Nothing [] [])
+
+        it "returns a basic Complete Todo" $ do
+            parse "x 2016-02-18 A basic todo" `shouldBe` (Complete "A basic todo" "2016-02-18" Nothing [] [])
